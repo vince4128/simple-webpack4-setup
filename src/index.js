6e2cf5f654edb43f3484d94742b8f90a09ee9fd5
vince4128 <vrlt.vincent@gmail.com>
2018-04-06 16:40:52 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//styles import
+import style from "./styles/style.scss";
+
+alert("it works !");
+//coucou
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const config = {
+
+    entry: {
+        app: ['babel-polyfill','./src/index.js']
+    },
+
+    output: {
+        path: path.resolve('./dist'),
+        filename: '[name].[chunkhash].js',
+        publicPath:'./'
+    },
+
+    module: {
+        rules: [
+            
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use:['babel-loader']
+            },
+
+            {
+                test: /\.html$/,
+                use:[
+                    {
+                        loader:'html-loader',
+                        options:{minimize:true}
+                    }
+                ]
+            },
+
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+
+    plugins: [
+        new CleanWebpackPlugin(['dist/*'],{
+            dry:false,
+            exclude:['img','video']            
+        }),
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename:"./index.html"
+        }),
+        new MiniCssExtractPlugin({
+            filename:"[name].[chunkhash].css",
+            chunkFilename:"[id].css"
+        })
+    ],
+
+    devServer: {
+        port:3003,
+        contentBase: path.join(__dirname, 'dist'),
+        historyApiFallback:true
+    }
+
+}
+
+module.exports = config;
