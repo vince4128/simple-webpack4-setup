0fa6d375e5f9a3bdd409b82dcdef1da4ff7cb35e
vince4128 <vrlt.vincent@gmail.com>
2018-04-12 13:46:14 +0200

    react app set up

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
-//styles import
+// styles import (for webpack)
 import style from "./styles/style.scss";
+
+// React
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(reducers)}>
+      <App />
+    </Provider>
+    , document.querySelector('.app-container'));
